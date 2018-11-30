8c37c65c5336829996db14138dea1e45700f4e05
qadada <krimsonmc@gmail.com>
2018-11-30 09:12:51 -0500

    Login Design #1: Personal Information section done. Compact info should come later on today

--- app/src/main/java/com/example/qadada/usync/JoinUsync.java
+++ app/src/main/java/com/example/qadada/usync/JoinUsync.java
@@ -1,20 +1,28 @@
 package com.example.qadada.usync;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 import com.facebook.shimmer.ShimmerFrameLayout;
 
-public class JoinUsync extends AppCompatActivity implements Animation.AnimationListener {
+public class JoinUsync extends SwipeGestureDetector implements Animation.AnimationListener {
 
     ImageView header;
     Animation animFadeIn;
+    private GestureDetector gestureDetector;
+    View.OnTouchListener gestureListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +39,16 @@ public class JoinUsync extends AppCompatActivity implements Animation.AnimationL
         ShimmerFrameLayout container =
                 (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
         container.startShimmer();
+
+
     }
+
+    @Override
+    protected void onSwipeLeft() {
+        startActivity(new Intent(JoinUsync.this, PersonalInfo.class));
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
     @Override
     public void onAnimationEnd(Animation animation) {
     }
@@ -42,10 +59,13 @@ public class JoinUsync extends AppCompatActivity implements Animation.AnimationL
 
     }
 
+
     @Override
     public void onAnimationStart(Animation animation) {
         // TODO Auto-generated method stub
 
     }
+
+
 
 }
--- app/src/main/java/com/example/qadada/usync/PersonalInfo.java
+++ app/src/main/java/com/example/qadada/usync/PersonalInfo.java
@@ -7,24 +7,40 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import com.example.qadada.usync.LoginScreen;
 
 
-public class PersonalInfo extends AppCompatActivity {
+public class PersonalInfo extends AppCompatActivity implements Animation.AnimationListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
-        TextView user = (TextView) findViewById(R.id.description);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "Cabin-Bold.ttf");
+        TextView user = (TextView) findViewById(R.id.pi);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Cabin.ttf");
         user.setTypeface(typeface);
         DatePicker simpleDatePicker = (DatePicker) findViewById(R.id.simpleDatePicker); // initiate a date picker4
         simpleDatePicker.updateDate(2000, 10, 6);
         simpleDatePicker.setSpinnersShown(true);
         simpleDatePicker.setCalendarViewShown(false);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
 
